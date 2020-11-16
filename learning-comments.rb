27ab1de9a80eee28c33b5ebc1334e097856a9d15
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-11-16 00:03:06 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,9 @@
+puts "Joy to the World"
+puts "All the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+#name = "Byron"
+name = "Luca"
+
+puts "We're sorry, but per health inspectors rules, #{name} is not allowed in the store."
